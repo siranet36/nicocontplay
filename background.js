125f9413e9beac338ee644910ecf3466a5350713
siranet36 <siranet36@gmail.com>
2017-02-09 22:48:29 +0900

    add

---
+++ background.js
@@ -0,0 +1,218 @@
+var pollInterval = 1;  // minute
+var requestTimeout = 1000 * 2;  // 2 seconds
+var tabURLs = {};
+var commURLs = {};
+var liveURLs = {};
+
+function startRequest(params) {
+  console.log('startRequest');
+  if (params && params.scheduleRequest) scheduleRequest();
+
+  for (var tabid in tabURLs) {
+    getCommURL(tabid);
+    getLiveURL(tabid);
+  }
+}
+
+function getCommURL(tabid) {
+  var tabURL_ = tabURLs[tabid];
+  console.log("getCommURL "+ tabid + " " + tabURL_ );
+  if (!commURLs[tabURL_]) {
+    console.log("chrome.tabs.sendMessage "+ tabid);
+    chrome.tabs.sendMessage(parseInt(tabid, 10), { tabid : tabid, taburl : tabURL_ }, function(response) {
+      if (response) {
+        commURLs[response.taburl] = response.url;
+
+        var tabid = response.tabid;
+        getLiveURL(tabid);
+      }
+    });
+  }
+}
+
+function getFeedUrl(url) {
+  return "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22"+encodeURIComponent(url)+"?rnd="+new Date().getTime()+"%22%20and%20xpath%3D%22%2F%2Fa%5B%40class%3D'now_live_inner'%5D%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+}
+
+function getLiveURL(tabid) {
+  console.log('getLiveURL ' + tabid);
+
+  var tabURL_ = tabURLs[tabid];
+  var commURL_ = commURLs[tabURL_];
+
+  var xhr = new XMLHttpRequest();
+  var abortTimerId = window.setTimeout(function() {
+    xhr.abort();
+  }, requestTimeout);
+
+  var liveURL_;
+
+  function handleSuccess() {
+    console.log('handleSuccess');
+    window.clearTimeout(abortTimerId);
+    liveURLs[commURL_] = liveURL_;
+
+    var tabURL__ = tabURLs[tabid];
+    var commURL__ = commURLs[tabURL__];
+    var liveURL__ = liveURLs[commURL__];
+
+    console.log('liveURLs  ['+commURL__+'] ' + liveURL__);
+    if (liveURL__ && liveURL__ !== tabURL__) {
+      console.log("tabs.update " + tabid);
+      chrome.tabs.update(parseInt(tabid, 10), { url: liveURL__ });
+    }
+  }
+
+  try {
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState != 4)
+        return;
+
+      var result = JSON.parse(xhr.responseText).query.results;
+      if (result !== null) {
+          liveURL_ = result.a[0].href.replace(/\?.*$/,"");
+          handleSuccess();
+      }
+    };
+
+    xhr.onerror = function(error) {
+      handleError();
+    };
+
+    xhr.open("GET", getFeedUrl(commURL_), true);
+    xhr.send(null);
+  } catch(e) {
+    console.error("gmailcheck_exception ", e);
+    handleError();
+  }
+}
+
+function scheduleRequest() {
+  console.log('scheduleRequest');
+  var delay = pollInterval;
+  console.log('Scheduling for: ' + delay);
+  console.log('Creating alarm');
+  chrome.alarms.create('refresh', {periodInMinutes: delay});
+}
+
+function onAlarm(alarm) {
+  console.log('Got alarm', alarm);
+  if (alarm && alarm.name == 'watchdog') {
+    onWatchdog();
+  } else {
+    startRequest({scheduleRequest:true});
+  }
+}
+
+function onWatchdog() {
+  console.log('onWatchdog');
+
+  chrome.tabs.query({},function(tabs){
+    tabs.forEach(function(tab){
+      var count = 0;
+      for (var tabid in tabURLs) {
+        console.log('tabid '+tabid +' tab.id '+tab.id);
+        if (tabid == tab.id){
+          count++;
+        }
+      }
+      if (count == 0) {
+        console.log('clear '+tab.id);
+        var tabURL_ = tabURLs[tab.id];
+        var commURL_ = commURLs[tabURL_];
+        if (tab.id in tabURLs) {
+          delete tabURLs[tabid];
+        }
+        if (tabURL_ in commURLs) {
+          delete commURLs[tabURL_];
+        }
+        if (commURL_ in liveURLs) {
+          delete liveURLs[commURL_];
+        }
+      }
+    });
+  });
+
+  chrome.alarms.get('refresh', function(alarm) {
+    if (alarm) {
+      console.log('Refresh alarm exists. Yay.');
+    } else {
+      console.log('Refresh alarm doesn\'t exist!? ' +
+                  'Refreshing now and rescheduling.');
+      startRequest({scheduleRequest:true});
+    }
+  });
+}
+
+
+chrome.runtime.onInstalled.addListener(function() {
+  console.log('chrome.runtime.onInstalled.addListener');
+
+  if (!chrome.alarms.onAlarm.hasListener(onAlarm)) {
+    chrome.alarms.onAlarm.addListener(onAlarm);
+  }
+  startRequest({scheduleRequest:true});
+  chrome.alarms.create('watchdog', {periodInMinutes:5});
+});
+
+function manageTab(tab) {
+  var tabid = tab.id;
+  console.log('manageTab ' + tabid);
+  var tabURL;
+  if (tab.id && tab.url) {
+    tabURL = tab.url.toString().replace(/\?.*$/,"");
+    console.log('tabURL ' + tabURL);
+    if (tabURL && tabURL.match(/http:\/\/live.nicovideo.jp\/watch\/(lv\d+)/)) {
+      if (tabURLs[tabid] !== tabURL) {
+        var tabURL_ = tabURLs[tabid];
+        var commURL_ = commURLs[tabURL_];
+
+        if (tabURL_ in commURLs) {
+          delete commURLs[tabURL_];
+        }
+        if (commURL_ in liveURLs) {
+          delete liveURLs[commURL_];
+        }
+
+        tabURLs[tabid] = tabURL;
+      }
+    } else {
+      var tabURL_ = tabURLs[tabid];
+      var commURL_ = commURLs[tabURL_];
+
+      if (tabid in tabURLs) {
+        delete tabURLs[tabid];
+      }
+      if (tabURL_ in commURLs) {
+        delete commURLs[tabURL_];
+      }
+      if (commURL_ in liveURLs) {
+        delete liveURLs[commURL_];
+      }
+    }
+  }
+}
+
+chrome.tabs.onUpdated.addListener(function(tabid, changeInfo, tab) {
+  console.log('chrome.tabs.onUpdated ' + tabid + " " + changeInfo.status );
+  if ( changeInfo.status === "complete") {
+    manageTab(tab);
+  }
+});
+
+chrome.tabs.onRemoved.addListener(function(tabid, removeInfo) {
+  console.log('chrome.tabs.onRemoved ' + tabid);
+  var tabURL_ = tabURLs[tabid];
+  var commURL_ = commURLs[tabURL_];
+
+  if (tabid in tabURLs) {
+    delete tabURLs[tabid];
+  }
+  if (tabURL_ in commURLs) {
+    delete commURLs[tabURL_];
+  }
+  if (commURL_ in liveURLs) {
+    delete liveURLs[commURL_];
+  }
+});
+
---
+++ contents.js
@@ -0,0 +1,27 @@
+function getCommURL() {
+  console.log("getCommURL");
+  var url;
+  var elements = document.getElementsByClassName('commu_info') ;
+  if (elements.length > 0) {
+    var elements2 = elements[0].getElementsByTagName('a');
+    url = elements2[0].href;
+  } else {
+    elements = document.getElementsByClassName('smn') ;
+    var elements2 = elements[0].getElementsByTagName('a');
+    url = elements2[0].href;
+  }
+
+  console.log("url "+url);
+  return url;
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+debugger;
+  console.log(request);
+  var tabid = request.tabid;
+  var taburl = request.taburl;
+  var url = getCommURL();
+  var response = {tabid : tabid, taburl: taburl, url: url};
+  sendResponse(response);
+  return true;
+});
